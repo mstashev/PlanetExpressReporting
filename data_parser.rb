4128cd32a73ff6b3190b52199c98e724dcdb3aa9
Michael Stashevsky <mstashev@gmail.com>
2017-02-02 17:26:25 -0500

    added file

--- data_parser.rb
+++ data_parser.rb
@@ -2,36 +2,55 @@ require 'csv'
 
 class Delivery
 
-  attr_accessor :dest, :shipped_item, :num_crates, :money_made
+  attr_accessor :pilot, :destination, :what_got_shipped, :number_of_crates, :money_we_made
 
-  def initialize(dest, shipped_item, num_crates, money_made)
-    @dest = dest
-    @shipped_item = shipped_item
-    @num_crates = num_crates
-    @money_made = money_made
+  def initialize(destination:, what_got_shipped:, number_of_crates:, money_we_made:)
+    @destination      = destination.to_s
+    @what_got_shipped = what_got_shipped.to_s
+    @number_of_crates = number_of_crates.to_i
+    @money_we_made    = money_we_made.to_f
+    @pilot            = setPilot
   end
 
+  def setPilot
+    case destination
+    when "Earth" then @pilot = "Fry"
+    when "Mars" then @pilot = "Amy"
+    when "Uranus" then @pilot = "Bender"
+    else @pilot = "Leela"
+    end
+  end
 end
 
-class Pilot
+daily_income, deliveries = [], []
 
-  attr_accessor :name, :route, :bonus, :trips
-
-  def initialize(name, route, bonus, trips)
-    @name = name
-    @route = route
-    @bonus = bonus
-    @trips = trips
-  end
-
-  def add_trip
-    @trip = trip + 1
-  end
-
-  def total_bonus(trip_bonus)
-    @bonus = trip_bonus.inject(0){|sum,item| sum + item}
-  end
-
-
-
+CSV.foreach("planet_express_logs.csv", headers: true, header_converters: :symbol) do |row|
+  delivery = Delivery.new(row)
+  puts delivery.inspect
+  deliveries << delivery
 end
+
+deliveries.each{ |delivery| daily_income << delivery.money_we_made}
+
+# deliveries.each do |delivery|
+#   case delivery.destination
+#   when "Earth"
+#     pilots["Fry"] << [delivery.destination, delivery.money_we_made]
+#   when "Mars"
+#     pilots["Amy"] << [delivery.destination, delivery.money_we_made]
+#   when "Uranus"
+#     pilots["Bender"] << [delivery.destination, delivery.money_we_made]
+#   else
+#     pilots["Leela"] << [delivery.destination, delivery.money_we_made]
+#   end
+# end
+
+weekly_income = daily_income.inject(0){|sum,item| sum + item}
+# puts weekly_income
+# puts pilots.inspect
+
+
+
+
+
+  #
