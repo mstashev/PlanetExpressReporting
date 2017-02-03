8e5c9c9f597d6738286fc72a0713db62c00a393c
Michael Stashevsky <mstashev@gmail.com>
2017-02-02 22:41:58 -0500

    Explorer Mode finished

--- data_parser.rb
+++ data_parser.rb
@@ -1,5 +1,4 @@
 require 'csv'
-
 class Delivery
 
   attr_accessor :pilot, :destination, :what_got_shipped, :number_of_crates, :money_we_made
@@ -8,7 +7,7 @@ class Delivery
     @destination      = destination.to_s
     @what_got_shipped = what_got_shipped.to_s
     @number_of_crates = number_of_crates.to_i
-    @money_we_made    = money_we_made.to_f
+    @money_we_made    = money_we_made.to_i
     @pilot            = setPilot
   end
 
@@ -20,6 +19,7 @@ class Delivery
     else @pilot = "Leela"
     end
   end
+
 end
 
 daily_income, deliveries = [], []
@@ -32,25 +32,26 @@ end
 
 deliveries.each{ |delivery| daily_income << delivery.money_we_made}
 
-# deliveries.each do |delivery|
-#   case delivery.destination
-#   when "Earth"
-#     pilots["Fry"] << [delivery.destination, delivery.money_we_made]
-#   when "Mars"
-#     pilots["Amy"] << [delivery.destination, delivery.money_we_made]
-#   when "Uranus"
-#     pilots["Bender"] << [delivery.destination, delivery.money_we_made]
-#   else
-#     pilots["Leela"] << [delivery.destination, delivery.money_we_made]
-#   end
-# end
+def get_pilot_profits(pilot, deliveries)
+  shipments = deliveries.select { |delivery| delivery.pilot.include? pilot }
+  shipments.collect{|delivery| delivery.money_we_made}.inject(0){|sum,item| sum + item}
+end
+
+def get_planet_profits(destination, deliveries)
+  shipments = deliveries.select { |delivery| delivery.destination.include? destination }
+  shipments.collect{|delivery| delivery.money_we_made}.inject(0){|sum,item| sum + item}
+end
+pilots = deliveries.flat_map{|delivery| delivery.pilot}.uniq
+destinations = deliveries.flat_map{|delivery| delivery.destination}.uniq
+
+profit_by_pilots = pilots.collect { |pilot| {pilot: pilot, profits: get_pilot_profits(pilot, deliveries)}}
+
+profit_by_planets = destinations.collect { |destination| {destination: destination, profits: get_planet_profits(destination, deliveries)}}
 
 weekly_income = daily_income.inject(0){|sum,item| sum + item}
-# puts weekly_income
-# puts pilots.inspect
 
+pilots.each { |pilot| puts "#{pilot}'s trips are: #{deliveries.count{|delivery| delivery.pilot == pilot}}" }
 
+profit_by_pilots.each { |profit_by_pilot| puts "#{profit_by_pilot[:pilot]}'s bonus is: #{profit_by_pilot[:profits]*0.1}" }
 
-
-
-  #
+profit_by_planets.each { |profit_by_planet| puts "#{profit_by_planet[:destination]}'s profits were: #{profit_by_planet[:profits]}" }
